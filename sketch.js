0c078395221e9c7966349a6700f2a892559bf971
EthanLJudice <124456727+EthanLJudice@users.noreply.github.com>
2024-08-28 13:14:59 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+function setup() {
+  createCanvas(500, 500);
+  fill(0);
+  ellipse(250, 250, 1000, 1000);
+  fill(255);
+  ellipse(0, 0, 207, 207);
+  ellipse(500, 0, 207, 207);
+  ellipse(0, 500, 207, 207);
+  ellipse(500, 500, 207, 207);
+  fill(0);
+  ellipse(0, 0, 150, 200);
+  ellipse(500, 0, 200, 150);
+  ellipse(0, 500, 200, 150);
+  ellipse(500, 500, 150, 200);
+  fill(255);
+  ellipse(250, 250, 500, 500);
+  fill(0);
+  ellipse(250, 250, 300, 50);
+  ellipse(-50, 250, 300, 150);
+  ellipse(550, 250, 300, 150);
+  ellipse(250, 250, 50, 300);
+  ellipse(250, 550, 150, 300);
+  ellipse(250, -50, 150, 300);
+  fill(255);
+  ellipse(250, 250, 70, 70);
+  fill(0);
+  ellipse(250, 250, 50, 50);
+  fill(255);
+  ellipse(-50, 250, 299, 100);
+  ellipse(550, 250, 299, 100);
+  ellipse(250, 550, 100, 299);
+  ellipse(250, -50, 100, 299);
+  fill(0);
+  ellipse(150, 150, 150, 150);
+  ellipse(350, 150, 150, 150);
+  ellipse(150, 350, 150, 150);
+  ellipse(350, 350, 150, 150);
+  fill(255);
+  ellipse(150, 150, 150, 50);
+  ellipse(350, 150, 50, 150);
+  ellipse(150, 350, 50, 150);
+  ellipse(350, 350, 150, 50);
+}
+
+function draw() {
+  // Put things you want to over and over here. 
+}
